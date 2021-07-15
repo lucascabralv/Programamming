8c2b2154e2af2c259bdb5dacdfbbee713e177cd6
Lucas Cabral <lucascabralvieira@gmail.com>
2021-07-15 17:00:37 -0300

    jogo-advinhacao.cpp

---
+++ C++/jogo-advinhacao.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void bemVindo(){
+    cout << "****************************************" << endl;
+    cout << "*** Bem-vindos ao jogo da advinhação ***" << endl;
+    cout << "****************************************" << endl;
+}
+
+int main(){
+    bemVindo();
+    
+    return 0;
+}
